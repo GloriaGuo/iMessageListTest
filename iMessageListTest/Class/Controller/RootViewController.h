686a915f1d22c7b83934688981e350cf5a36d07d
GloriaGuo <iouurg@gmail.com>
2013-04-27 08:07:14 +0800

    fix the project settings

---
+++ iMessageListTest/Class/Controller/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  iMessageListTest
+//
+//  Created by Gloria Guo on 4/27/13.
+//  Copyright (c) 2013 Gloria Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ iMessageListTest/Class/Controller/RootViewController.m
@@ -0,0 +1,43 @@
+//
+//  RootViewController.m
+//  iMessageListTest
+//
+//  Created by Gloria Guo on 4/27/13.
+//  Copyright (c) 2013 Gloria Guo. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
